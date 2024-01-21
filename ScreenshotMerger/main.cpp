7dc9c9dcbf3c428cea47d1ce70081e8e06625ca0
Andrei Muntean <andrei.muntean.dev@gmail.com>
2024-01-21 20:07:32 +0000

    Initial Commit

---
+++ ScreenshotMerger/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ScreenshotMerger
+//
+//  Created by Andrei Muntean on 21/01/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
