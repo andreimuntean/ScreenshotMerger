cc90315400edf057f50220722ba196b474c7be02
Andrei Muntean <andrei.muntean.dev@gmail.com>
2024-02-10 22:34:54 +0000

    Update main() to also monitor and output performance statistics

--- ScreenshotMerger/main.cpp
+++ ScreenshotMerger/main.cpp
@@ -5,6 +5,7 @@
 //  Created by Andrei Muntean on 21/01/2024.
 //
 
+#include <chrono>
 #include <iostream>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -12,7 +13,9 @@
 #include "ScreenshotMerger.hpp"
 
 int main(int argc, const char * argv[]) {
-    if (argc == 1) {
+    using namespace std::literals;
+
+    if (argc <= 2) {
         std::cerr << "Please provide at least two images as input and specify an output path, for example:\n";
         std::cerr << "  ScreenshotMerger C:/Images/First.png C:/Images/Second.png C:/Images/Output.png\n";
         return 0;
@@ -20,15 +23,21 @@ int main(int argc, const char * argv[]) {
 
     Image result(cv::imread(argv[1], cv::IMREAD_COLOR));
     std::cout << "Merging " << result.Width() << 'x' << result.Height() << '\n';
+    std::chrono::duration<double> timeElapsedMerging = 0s;
 
     for (int i = 2; i < argc - 1; ++i) {
         Image image = Image(cv::imread(argv[i], cv::IMREAD_COLOR));
-        std::cout << "      + " << image.Width() << 'x' << image.Height() << "...\n";
+        std::cout << "      + " << image.Width() << 'x' << image.Height() << "...";
 
+        auto startTime = std::chrono::high_resolution_clock::now();
         result = ScreenshotMerger::Merge(result, image);
+        auto timeElapsed = std::chrono::high_resolution_clock::now() - startTime;
+        timeElapsedMerging += timeElapsed;
+        std::cout << " Done (took " << timeElapsed / 1ms << " milliseconds)\n";
     }
 
-    std::cout << "Result is a " << result.Width() << 'x' << result.Height() << " image\n";
+    std::cout << "Result is a " << result.Width() << 'x' << result.Height()
+              << " image (took " << timeElapsedMerging / 1ms << " milliseconds).\n";
     cv::imwrite(argv[argc - 1], result.ToMat());
 
     return 0;
